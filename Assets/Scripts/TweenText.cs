8a61ed2e6d7a5e15de6847c8d48be3cdf3b54d44
poperinA <falishaakhan05@gmail.com>
2024-02-13 10:37:05 +0800

    Main Screen UI

---
+++ Assets/Scripts/TweenText.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class TweenText : MonoBehaviour
+{
+    public float scaleFactor;
+    public float tweenTime;
+
+    void Start()
+    {
+
+    }
+
+    public void ScaleUp()
+    {
+        LeanTween.cancel(gameObject);
+
+        // Initial scale
+        transform.localScale = Vector3.one;
+
+        // Tween to the target scale
+        LeanTween.scale(gameObject, Vector3.one * scaleFactor, tweenTime)
+            .setEase(LeanTweenType.easeOutExpo);
+    }
+
+    public void ScaleDown()
+    {
+        LeanTween.cancel(gameObject);
+
+        // Tween from current scale to the original scale (Vector3.one)
+        LeanTween.scale(gameObject, Vector3.one, tweenTime)
+            .setEase(LeanTweenType.easeOutExpo);
+    }
+
+}
