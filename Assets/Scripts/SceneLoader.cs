a5407d281358ae020682f7fe678d1844db1f8ab6
poperinA <falishaakhan05@gmail.com>
2024-02-14 17:03:45 +0800

    added a scenemanager

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -11,4 +11,39 @@ public class SceneLoader : MonoBehaviour
         // Load the "Main Menu" scene
         SceneManager.LoadScene("Main Menu");
     }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Game Select");
+    }
+
+    public void Settings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void BackToGame()
+    {
+        SceneManager.LoadScene("Room");
+    }
+
+    public void BackToGameSelect()
+    {
+        SceneManager.LoadScene("Game Select");
+    }
+
+    public void CreateGame()
+    {
+        SceneManager.LoadScene("New Game");
+    }
+
+    public void GoToGame()
+    {
+        SceneManager.LoadScene("Room");
+    }
 }
