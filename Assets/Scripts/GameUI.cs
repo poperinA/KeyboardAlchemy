0978d35a02b34fba0b5777f431b07def22766d87
poperinA <falishaakhan05@gmail.com>
2024-02-15 18:03:27 +0800

    UI for room scene

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -3,7 +3,16 @@ using UnityEngine;
 public class GameUI : MonoBehaviour
 {
     public GameObject Options;
+    public GameObject Settings;
+    public GameObject AreYouSure;
 
+    public PlayerController playerControllerM;
+    public PlayerController playerControllerD;
+
+    public void Update()
+    {
+        PauseCheck();
+    }
     public void ToggleOptions()
     {
         if (Options != null)
@@ -11,6 +20,80 @@ public class GameUI : MonoBehaviour
             bool isActive = Options.activeSelf;
 
             Options.SetActive(!isActive);
+        }
+    }
+
+    public void PauseCheck()
+    {
+        if (Options.activeInHierarchy == true || AreYouSure.activeInHierarchy == true)
+        {
+            Pause();
+        }
+        else
+        {
+            Resume();
+        }
+    }
+
+    private void Pause()
+    {
+
+        if (playerControllerM != null && playerControllerM.enabled == true)
+        {
+            playerControllerM.enabled = false;
+        }
+
+        if (playerControllerD != null && playerControllerD.enabled == true)
+        {
+            playerControllerD.enabled = false;
+        }
+    }
+
+    private void Resume()
+    {
+
+        if (playerControllerM != null && playerControllerM.enabled == false)
+        {
+            playerControllerM.enabled = true;
+        }
+
+        if (playerControllerD != null && playerControllerD.enabled == false)
+        {
+            playerControllerD.enabled = true;
+        }
+    }
+
+    public void OpenSettings()
+    {
+        if (Settings != null)
+        {
+            Settings.SetActive(true);
+        }
+    }
+
+    public void CloseSettings()
+    {
+        if (Settings != null)
+        {
+            Settings.SetActive(false);
+        }
+    }
+
+    public void OpenAreYouSure()
+    {
+        if (AreYouSure != null)
+        {
+            Options.SetActive(false);
+            AreYouSure.SetActive(true);
+        }
+    }
+
+    public void CloseAreYouSure()
+    {
+        if (AreYouSure != null)
+        {
+            AreYouSure.SetActive(false);
+            Options.SetActive(true);
         }
     }
 }
