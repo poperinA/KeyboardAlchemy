444a36d43e1d6beb5cab082c63e8bffad1664757
poperinA <falishaakhan05@gmail.com>
2024-02-22 18:55:07 +0800

    scene transition working

--- Assets/Scripts/UI/SceneLoader1.cs
+++ Assets/Scripts/UI/SceneLoader1.cs
@@ -3,12 +3,31 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SceneLoader1 : MonoBehaviour
 {
+    public GameObject Black;
     public Animator transition;
     public float transitionTime;
+
+    void Start()
+    {
+        // Get the name of the current scene
+        string sceneName = SceneManager.GetActiveScene().name;
+
+        // Check if the scene name is "Room"
+        if (sceneName == "Room")
+        {
+            // Enable the black GameObject
+            Black.SetActive(true);
+        }
+    }
     public void GoToGame()
     {
-        LoadNextLevel();
-        AudioManager.Instance.PlaySFX("BtnClick");
+        if(Black.activeInHierarchy == false)
+        {
+            Black.SetActive(true);
+            LoadNextLevel();
+            AudioManager.Instance.PlaySFX("BtnClick");
+        }
+
     }
 
     public void LoadNextLevel()
